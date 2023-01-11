c098c73badba9cdc2c5413539ce1b3beec84c225
imvickykumar999 <imvickykumar999@gmail.com>
2023-01-12 02:39:11 +0530

    done

--- table2csv.py
+++ table2csv.py
@@ -1,43 +1,4 @@
 
-def fetchtags():
-    from bs4 import BeautifulSoup as bs
-    import requests
-
-    link = 'https://up-rera.in/AgentDetails'
-    req = requests.get(link)
-
-    soup = bs(req.content, 'html5lib')
-    box = soup.findAll('div', attrs = {'class':'container-fluid'}) 
-
-    k = box[3].findAll('div', attrs = {'class':'col-sm-2'})
-    # v = box[3].findAll('div', attrs = {'class':'col-sm-4'})
-
-    mylst=[]
-    myval=[]
-    for i in k:
-        mylst.append(i.text.split(' :')[0])
-        myval.append('Value')
-
-    out = dict(zip(mylst, myval))
-    # print(out)
-
-    import pandas as pd
-    df = pd.DataFrame(out, index=[0])
-    print(df)
-    df.to_csv('output.csv')
-
-
-    # for i in v:
-    #     print(i.text)
-
-    # r = box[3].findAll('span', attrs = {'id':'ctl00_ContentPlaceHolder1_lblbankname'})
-    # l = r[0]
-    # print(l)
-
-# fetchtags()
-
-
-
 def automatetab():
     from selenium import webdriver
     from selenium.webdriver.common.by import By
@@ -47,7 +8,7 @@ def automatetab():
     lst = []
 
     # for i in range(2, 5778):
-    for i in range(2, 5):
+    for i in range(2, 45):
         if i < 10:
             i = f'0{i}'
         else:
@@ -55,17 +16,24 @@ def automatetab():
         ViewDetails = driver.find_element(By.XPATH, f'//*[@id="ctl00_ContentPlaceHolder1_grdagents_ctl{i}_lnkView"]')
         ViewDetails.click()
 
-        
         driver.switch_to.window(driver.window_handles[int(i)-1])
         elements = driver.find_element(By.XPATH, '//*[@id="main"]/section[2]/div/div/div/div/div[2]/div/div/div')
         mylst = elements.text.split('\n')
-        print(mylst)
 
+        for i in range(len(mylst)):
+            try:
+                if (':' in mylst[i]) and (':' in mylst[i+1]):
+                    i+=1
+                    mylst.insert(i, 'Null')
+            except:
+                pass
+
+        print(mylst)
         lst.append(mylst[1::2])
         driver.switch_to.window(driver.window_handles[0])
 
     import pandas as pd
     df = pd.DataFrame(lst, columns = mylst[::2])
-    print(df)
+    df.to_csv('output.csv')
 
 automatetab()

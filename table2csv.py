f884bc8ed699cbb3695c011201fd59184226d0d2
imvickykumar999 <imvickykumar999@gmail.com>
2023-01-12 14:49:09 +0530

    done

--- table2csv.py
+++ table2csv.py
@@ -1,14 +1,14 @@
 
-def automatetab():
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+def automatetab():
     driver = webdriver.Chrome()
     driver.get('https://up-rera.in/agents')
-    lst = []
+    file = open('output.csv', 'w')
 
     # for i in range(2, 5778):
-    for i in range(2, 45):
+    for i in range(2, 15):
         if i < 10:
             i = f'0{i}'
         else:
@@ -16,24 +16,27 @@ def automatetab():
         ViewDetails = driver.find_element(By.XPATH, f'//*[@id="ctl00_ContentPlaceHolder1_grdagents_ctl{i}_lnkView"]')
         ViewDetails.click()
 
-        driver.switch_to.window(driver.window_handles[int(i)-1])
+        try:
+            driver.switch_to.window(driver.window_handles[int(i)-1])
+        except:
+            pass
+
         elements = driver.find_element(By.XPATH, '//*[@id="main"]/section[2]/div/div/div/div/div[2]/div/div/div')
         mylst = elements.text.split('\n')
 
-        for i in range(len(mylst)):
+        for j in range(len(mylst)):
             try:
-                if (':' in mylst[i]) and (':' in mylst[i+1]):
-                    i+=1
-                    mylst.insert(i, 'Null')
+                if (':' in mylst[j]) and (':' in mylst[j+1]):
+                    j+=1
+                    mylst.insert(j, 'Null')
             except:
                 pass
 
-        print(mylst)
-        lst.append(mylst[1::2])
+        if i=='2':
+            file.write(', '.join(mylst[::2])+'\n')
+        else:
+            file.write(', '.join(mylst[1::2])+'\n')
         driver.switch_to.window(driver.window_handles[0])
-
-    import pandas as pd
-    df = pd.DataFrame(lst, columns = mylst[::2])
-    df.to_csv('output.csv')
+    file.close()
 
 automatetab()

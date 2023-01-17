347be455c718fff607a2563564199d27d1ee9b1e
imvickykumar999 <imvickykumar999@gmail.com>
2023-01-17 16:01:04 +0530

    txt to csv

--- table2csv.py
+++ table2csv.py
@@ -1,25 +1,27 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import csv
 
 def automatetab():
     driver = webdriver.Chrome()
     driver.get('https://up-rera.in/agents')
-    file = open('output.csv', 'w')
 
-    # for i in range(2, 5778):
-    for i in range(2, 15):
+    with open('output.txt', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = list(reader)
+        row_count = len(data)
+
+    file = open(f'output.txt', 'a')
+    for i in range(row_count, 5778):
         if i < 10:
             i = f'0{i}'
         else:
             i = f'{i}'
+
         ViewDetails = driver.find_element(By.XPATH, f'//*[@id="ctl00_ContentPlaceHolder1_grdagents_ctl{i}_lnkView"]')
         ViewDetails.click()
-
-        try:
-            driver.switch_to.window(driver.window_handles[int(i)-1])
-        except:
-            pass
+        driver.switch_to.window(driver.window_handles[-1])
 
         elements = driver.find_element(By.XPATH, '//*[@id="main"]/section[2]/div/div/div/div/div[2]/div/div/div')
         mylst = elements.text.split('\n')
@@ -32,6 +34,7 @@ def automatetab():
             except:
                 pass
 
+        print(mylst)
         if i=='2':
             file.write(', '.join(mylst[::2])+'\n')
         else:

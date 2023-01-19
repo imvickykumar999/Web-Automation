46bef3a1347826abea19fa1fcd5391bae7e9485c
imvickykumar999 <imvickykumar999@gmail.com>
2023-01-20 00:36:01 +0530

    final

--- BeautifulSoup WebScrapping/classify.py
+++ BeautifulSoup WebScrapping/classify.py
@@ -1,42 +1,47 @@
 
 import pandas as pd
-from bs4 import BeautifulSoup as bs
-import requests
 
 link = 'https://en.wikipedia.org/wiki/Mobile_telephone_numbering_in_India'
+pairs = {
+    0 : 'Telecom circles',
+    1 : 'Network operators',
+    3 : '9 series',
+    4 : '8 series',
+    5 : '7 series',
+    6 : '6 series'
+}
 
 
-def fetch():
-    req = requests.get(link)
-    soup = bs(req.content, 'html5lib')
+def save_tables(i):
+    df = pd.read_html(link)[i]
+    df.to_csv(f"data/{pairs[i]}.csv", index=False)
+    print(df.iloc[:,:2])
+    print('-'*40)
 
-    box = soup.findAll('table', attrs = {'class':'wikitable'}) 
-    print(box[1])
-
-# fetch()
-
-
-def save():
-    df = pd.read_html(link)[4]
-    print(df)
-    # df.to_csv("8 series.csv", index=False)
-
-# save()
+save_tables(0)
+save_tables(1)
 
 
-def readcsv(phno='8239957923'):
-    csvFile = pd.read_csv('8 series.csv')
+def readcsv():
+    print('Index, Series, Operator, Circle	')
+    print('-'*40)
+
     csv_phno = pd.read_csv('sample phno.csv')
-    print(str(csv_phno.iloc[0,0]))
+    file = open('OUTPUT.csv', 'a')
 
-    for i in range(len(csvFile.index)):
-        for j in range(len(csvFile.iloc[i, :])):
-            if (len(str(csvFile.iloc[i, :][j])) == 4) or (len(str(csvFile.iloc[i, :][j])) == 6):
-                try:
-                    if int(phno[:4]) == int(csvFile.iloc[i, :][j]):
-                        print(int(csvFile.iloc[i, :][j]), csvFile.iloc[i, :][j+1], csvFile.iloc[i, :][j+2])
-                except:
-                    pass
+    for k in range(len(csv_phno.index)):
+        phno = str(csv_phno.iloc[k,0])
+        csvFile = pd.read_csv(f'data/{pairs[12 - int(phno[0])]}.csv')
 
-phno = '8239957923'
-readcsv(phno)
+        for i in range(len(csvFile.index)):
+            for j in range(len(csvFile.iloc[i, :])):
+
+                if (len(str(csvFile.iloc[i, :][j])) == 4) or (len(str(csvFile.iloc[i, :][j])) == 6):
+                    try:
+                        if int(phno[:4]) == int(csvFile.iloc[i, :][j]):
+                            file.write(f'{csv_phno.iloc[k,0]}, {csvFile.iloc[i, :][j+1]}, {csvFile.iloc[i, :][j+2]}' + '\n')
+                            print(f'{k}/{len(csv_phno.index)}, {int(csvFile.iloc[i, :][j])} {phno[4:]}, {csvFile.iloc[i, :][j+1]}, {csvFile.iloc[i, :][j+2]}')
+                    except:
+                        pass
+    file.close()
+readcsv()
